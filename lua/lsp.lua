fe4de403196bc071217e0eacbb27c17c9121f948
George Smith <georgepetersmith@outlook.com>
2025-06-24 23:46:22 +0100

    update

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,3 +1,8 @@
+vim.lsp.enable({
+  "omnisharp",
+  "rust_analyzer",
+})
+
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,37 +1,3 @@
 return {
-  "williamboman/mason-lspconfig.nvim",
-  event = { 'BufReadPre', 'BufNewFile' },
-  dependencies = {
-    'neovim/nvim-lspconfig',
-    {
-      "williamboman/mason.nvim",
-      cmd = { 'Mason' },
-      lazy = true,
-      opts = {
-        registries = {
-          "github:mason-org/mason-registry",
-          "github:Crashdummyy/mason-registry",
-        },
-      },
-    },
-  },
-  opts = {
-    ensure_installed = {
-      "angularls@15.2.1",
-      "html",
-      "cssls",
-      "tailwindcss",
-      "lua_ls",
-      "marksman",
-      'rust_analyzer',
-      'jsonls',
-      'ts_ls',
-      'powershell_es',
-    },
-    handlers = {
-      function(server_name)
-        require('lspconfig')[server_name].setup({})
-      end,
-    },
-  },
+  "neovim/nvim-lspconfig",
 }
