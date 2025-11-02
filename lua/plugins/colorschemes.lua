d9b8d1704d372ab1c9a4c67aae98d7a476398909
George Smith <georgepetersmith@outlook.com>
2025-11-02 10:27:27 +0000

    Update

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,3 +1,9 @@
 return {
   "Koalhack/darcubox-nvim",
+  {
+    "vague-theme/vague.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other plugins
+    opts = {},
+  },
 }
