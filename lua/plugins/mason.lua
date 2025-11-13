2b49518699d37ab9d6f61dc9b936d6898c6e3617
George Smith <georgepetersmith@outlook.com>
2025-11-13 23:14:46 +0000

    Update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,38 +1,48 @@
 return {
-  "mason-org/mason-lspconfig.nvim",
-  config = function()
-    require("mason").setup({
-      ensure_installed = {
-        "lua_ls",
-        "rust_analyzer",
-        "stylua",
-        "ts_ls",
-      },
-      registries = {
-        "github:mason-org/mason-registry",
-        "github:Crashdummyy/mason-registry",
-      },
-    })
-    require("mason-nvim-dap").setup({
-      ensure_installed = { "netcoredbg", "cppdbg" },
-      automatic_installation = true,
-      handlers = {
-        function(config)
-          require("mason-nvim-dap").default_setup(config) -- registers "cppdbg"
-        end,
-      },
-    })
-    require("mason-lspconfig").setup()
-  end,
+  "WhoIsSethDaniel/mason-tool-installer.nvim",
+  opts = {
+    ensure_installed = {
+      "netcoredbg",
+      "cppdbg",
+      "lua_ls",
+      "rust_analyzer",
+      "stylua",
+      "ts_ls",
+      "roslyn",
+    },
+  },
   dependencies = {
-    "neovim/nvim-lspconfig",
-    "jay-babu/mason-nvim-dap.nvim",
-    "mason-org/mason.nvim",
     {
-      "seblyng/roslyn.nvim",
-      ---@module 'roslyn.config'
-      ---@type RoslynNvimConfig
-      opts = {},
+      "mason-org/mason-lspconfig.nvim",
+      config = function()
+        require("mason").setup({
+          registries = {
+            "github:mason-org/mason-registry",
+            "github:Crashdummyy/mason-registry",
+          },
+        })
+        require("mason-nvim-dap").setup({
+          automatic_installation = true,
+          handlers = {
+            function(config)
+              require("mason-nvim-dap").default_setup(config) -- registers "cppdbg"
+            end,
+          },
+          ensure_installed = {},
+        })
+        require("mason-lspconfig").setup()
+      end,
+      dependencies = {
+        "neovim/nvim-lspconfig",
+        "jay-babu/mason-nvim-dap.nvim",
+        "mason-org/mason.nvim",
+        {
+          "seblyng/roslyn.nvim",
+          ---@module 'roslyn.config'
+          ---@type RoslynNvimConfig
+          opts = {},
+        },
+      },
     },
   },
 }
